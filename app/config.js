d3d943e6917d6667401fce993201ebead651993d
Ming Xu <gmbody7@gmail.com>
2015-11-07 15:40:12 -0800

    prepare collaborate

--- app/config.js
+++ app/config.js
@@ -1,6 +1,7 @@
 window.Config = {
-	baseUrl: "http://frontend.dev:7888/boluomi/frontend/web/index.php",
+	//baseUrl: "http://frontend.dev:7888/boluomi/frontend/web/index.php",
 	//baseUrl:"http://www.boluomi1314.com:8081",
+	baseUrl: "http://boluomi.dev:8081",
 	API:{
 		signupAPI:"http://boluomi.rjl.com/v1/register/signup",
 		loginAPI:"http://boluomi.rjl.com/v1/register/login"
