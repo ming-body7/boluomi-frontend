0bf754b5fe1ec6f7186bcef22d1aee893b476e4e
Ming Xu <gmbody7@gmail.com>
2016-04-09 22:53:02 -0700

    修改后台连接API

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 window.Config = {
     //baseUrl: "http://boluomi.dev:8081",
-    //baseUrl:"http://www.boluomi1314.com:8081",
-    baseUrl:"http://localhost",
+    baseUrl:"http://www.boluomi1314.com:8081",
+    //baseUrl:"http://localhost",
     //uploadAPI: "http://boluomi.dev:8083/upload.php",
     uploadAPI:"http://www.boluomi1314.com:8083/upload.php",
     //uploadFolder: "http://boluomi.dev:8083/data/upload/"
