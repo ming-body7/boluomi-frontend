c2e1537fff0d347943f577299f87f69c2bd5d0fd
kiven1989 <791474722@qq.com>
2016-04-20 21:07:08 +0800

    for pc and mobile

--- app/demo/js/base.js
+++ app/demo/js/base.js
@@ -1,5 +1,20 @@
-//设置页面固定显示宽度320;
-setDeviceW();
+function to_pc(){
+	var system ={};  
+	    var p = navigator.platform;       
+	    system.win = p.indexOf("Win") == 0;  
+	    system.mac = p.indexOf("Mac") == 0;  
+	    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);     
+	    if(system.win||system.mac||system.xll){//电脑
+	    	window.location.href = 'www.boluomi1314.com/demo/pc/web_share.html?pid='+ getQueryString(pid);
+	    }else{
+	    	//设置页面固定显示宽度320;
+	    	setDeviceW();
+	    }
+}
+
+//见设备是PC的  跳转到 www.boluomi1314.com/demo/pc/web_share.html?pid=18 
+to_pc();
+
 function setDeviceW(){
 	var w = $(window);
 	var h = w.height();
@@ -15,4 +30,17 @@ function setDeviceW(){
 	$("#eqMobileViewport").attr("content", "width=320, initial-scale=" + f + ", maximum-scale=" + f + ", user-scalable=no");
 	e && $(".edit_area").css({marginTop: e});
 	d && $(".edit_area").css({marginLeft: d});
-};		
+};	
+
+
+
+function getQueryString(key){
+   var search=window.location.search.substring(1).split('&');
+   for (var i=0; i<search.length; i++){
+       if (search[i].split('=')[0]==key){
+           return decodeURI(search[i].split('=')[1]);
+       }
+   }
+   return undefined;
+}
+
