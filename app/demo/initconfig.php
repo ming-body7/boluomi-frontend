cec1ceffb66787ae78d74b47a82e85568f507017
kiven1989 <791474722@qq.com>
2016-05-18 14:46:13 +0800

    updata weixin

--- app/demo/initconfig.php
+++ app/demo/initconfig.php
@@ -1,6 +1,6 @@
 <?php
 require_once "jssdk.php";
-$jssdk = new JSSDK("wxce5103688fc8d7e0", "1117e5e697bcfafadc1b91d726ed33b6");
+$jssdk = new JSSDK("wxa81810d5be83a1da", "5f7a3196ad2aaf421ce5b0de9195a3cd");
 $signPackage = $jssdk->GetSignPackage();
 
 echo json_encode(array(
